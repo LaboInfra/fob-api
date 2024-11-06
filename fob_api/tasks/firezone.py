8bd0e93caa7067abadb50f6dff496d2ce7b58282
msterhuj <gabin.lanore@gmail.com>
2024-11-06 11:29:39 +0100

    feat: implement rules sync to api

--- fob_api/tasks/firezone.py
+++ fob_api/tasks/firezone.py
@@ -1,6 +1,7 @@
 from firezone_client import generate_password, generate_key_pair
 from firezone_client import User as VpnUser
 from firezone_client import Device as VpnDevice
+from firezone_client import Rule as VpnRule
 from sqlmodel import Session, select
 
 from fob_api.models.user import User
@@ -25,6 +26,66 @@ def create_user(username: str):
             ))
             return create_user(username)
         return vpn_user.id
+
+@celery.task()
+def get_rules_for_user(username: str):
+    with Session(engine) as session:
+        user = session.exec(select(User).where(User.username == username)).first()
+        if not user:
+            raise Exception("User not found")
+        vpn_user_id: VpnUser = create_user(username)
+        rules = firezone_driver.list(VpnRule)
+        return [
+            {
+                "id": rule.id,
+                "destination": rule.destination,
+                "action": rule.action,
+                "user_id": rule.user_id
+            }
+            for rule in rules
+            if vpn_user_id == rule.user_id
+        ]
+
+@celery.task()
+def sync_user_rules(username: str):
+    """
+    Sync all rules
+        - delete rules that are not in allowed subnets
+        - delete all rules if user is disabled
+        - create rules for all allowed subnets
+    """
+    with Session(engine) as session:
+        user = session.exec(select(User).where(User.username == username)).first()
+        if not user:
+            raise Exception("User not found")
+        vpn_user_id: VpnUser = create_user(username)
+        allowed_subnets = user.allowed_subnets.split(",")
+        rules_in_project = get_rules_for_user(username)
+
+        # delete not wanted rules
+        for rule in rules_in_project:
+            if rule["destination"] not in allowed_subnets or user.disabled:
+                rule_id = rule["id"]
+                print(f"Deleting rule {rule_id} for user {username} as it is not in allowed subnets or user is disabled")
+                rule_obj = firezone_driver.get(VpnRule, id=rule["id"])
+                firezone_driver.delete(rule_obj)
+
+        # create new rules
+        for subnet in allowed_subnets:
+            if user.disabled:
+                break
+            if subnet == "":
+                continue
+            if not any(rule["destination"] == subnet for rule in rules_in_project):
+                print(f"Creating rule for subnet {subnet} for user {username}")
+                firezone_driver.create(VpnRule(
+                    user_id=vpn_user_id,
+                    destination=subnet,
+                    action="accept"
+                ))
+
+        return get_rules_for_user(username)
+
 
 @celery.task()
 def get_devices_for_user(user: User):
