6c402bcf04c898ec5bdf5cb07c5d75d2f8beefb9
msterhuj <gabin.lanore@gmail.com>
2024-11-19 22:42:03 +0100

    feat: add tables for user password reset requests

--- fob_api/models/user.py
+++ fob_api/models/user.py
@@ -4,9 +4,7 @@ from sqlmodel import Field, SQLModel
 
 class User(SQLModel, table=True):
     """
-    This class represents the User for all the system?
-
-    in this app user cant create account themselves, only admin can create account for them and they need to enable the account by themselves
+    This class represents the User for all the system of laboinfra
     """
     id: int = Field(primary_key=True)
     username: str
@@ -16,3 +14,14 @@ class User(SQLModel, table=True):
     disabled: bool = False
     last_synced: datetime = Field(default=datetime.now())
     allowed_subnets: str = Field(default="")
+
+class UserPasswordReset(SQLModel, table=True):
+    """
+    This class represents the UserPasswordReset request
+    """
+    id: int = Field(primary_key=True)
+    user_id: int = Field(foreign_key="user.id")
+    token: str
+    source_ip: str
+    created_at: datetime = Field(default=datetime.now())
+    expires_at: datetime
