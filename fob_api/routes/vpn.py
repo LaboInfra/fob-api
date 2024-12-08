c190ac9b45d9cfced38c8087a64e827e9c7b2c6a
msterhuj <gabin.lanore@gmail.com>
2024-12-08 23:21:38 +0100

    feat: add page to register device to headscale

--- fob_api/routes/vpn.py
+++ fob_api/routes/vpn.py
@@ -1,6 +1,8 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 from fob_api import auth
 from fob_api.models.database import User
@@ -9,33 +11,54 @@ from fob_api.tasks import headscale
 
 router = APIRouter(prefix="/devices")
 
-@router.get("/{username}", tags=["vpn"])
-def list_user_devices(user: Annotated[User, Depends(auth.get_current_user)], username: str):
-    """
-    List all devices for a user
-    """
-    if not user.is_admin and user.username != username:
-        raise HTTPException(status_code=403, detail="You are not an admin")
-    return None
+template = Jinja2Templates(directory="templates")
 
-@router.post("/{username}", tags=["vpn"])
-def create_device(user: Annotated[User, Depends(auth.get_current_user)], username: str, device: CreateDevice):
+@router.get("/register/{mkey}", tags=["vpn"], response_class=HTMLResponse)
+def register_device_get(request: Request, mkey: str):
     """
-    Create a new device for a user
+    Handle device registration for headscale require basic auth
     """
-    if not user.is_admin and user.username != username:
-        raise HTTPException(status_code=403, detail="You are not an admin")
-    return None
+    if not mkey.startswith("mkey:"):
+        raise HTTPException(status_code=400, detail="Invalid mkey")
+    return template.TemplateResponse(
+        request=request,
+        name="register_device.html.j2",
+        context={"mkey": mkey}
+    )
 
-@router.delete("/{username}/{device_id}", tags=["vpn"])
-def delete_device(user: Annotated[User, Depends(auth.get_current_user)], username: str, device_id: str):
+@router.post("/register", tags=["vpn"])
+async def register_device_post(request: Request):
     """
-    Delete a device for a user
+    Handle device registration for headscale require basic auth
     """
-    if not user.is_admin and user.username != username:
-        raise HTTPException(status_code=403, detail="You are not an admin")
-    devices = None
-    for device in devices:
-        if device.id == device_id:
-            return None
-    raise HTTPException(status_code=404, detail="Device not found")
+    form_data = await request.form()
+    mkey = form_data.get("mkey")
+    username = form_data.get("username")
+    password = form_data.get("password")
+    if not mkey or not username or not password:
+        return template.TemplateResponse(
+            request=request,
+            name="register_device.html.j2",
+            context={"mkey": mkey, "error": "All fields are required"}
+        )
+    if not mkey.startswith("mkey:"):
+        return template.TemplateResponse(
+            request=request,
+            name="register_device.html.j2",
+            context={"mkey": mkey, "error": "Invalid mkey"}
+        )
+    user = auth.basic_auth_validator(username, password)
+    if not user:
+        return template.TemplateResponse(
+            request=request,
+            name="register_device.html.j2",
+            context={"mkey": mkey, "error": "Invalid username or password"}
+        )
+    
+    # TODO add headscale call to validate mkey and register device
+
+    return template.TemplateResponse(
+        request=request,
+        name="register_device.html.j2",
+        context={"mkey": mkey, "success": "Device registered successfully you can now close this page"}
+    )
