b5d22b3a7efded94439a41af5cc9a7196767cc8b
Gabin L <gabin.lanore@gmail.com>
2025-02-02 22:53:43 +0100

    feat: add client for nova, neutron and cinder
    Signed-off-by: Gabin L <gabin.lanore@gmail.com>

--- fob_api/openstack.py
+++ fob_api/openstack.py
@@ -4,15 +4,15 @@ This module is used to authenticate with OpenStack and create a client object.
 
 from keystoneauth1.identity import v3
 from keystoneauth1 import session
-from keystoneclient.v3 import client
+from keystoneclient.v3 import client as keystone_client
+from novaclient import client as nova_client
+from neutronclient.v2_0 import client as neutron_client
+from cinderclient import client as cinder_client
 
 from fob_api import Config
 
+def get_session() -> session.Session:
 
-def get_keystone_client() -> client.Client:
-    """
-    Returns a keystone client object.
-    """
     config = Config()
 
     keystone_auth = v3.Password(
@@ -24,6 +24,28 @@ def get_keystone_client() -> client.Client:
         project_domain_name=config.os_project_domain_name
     )
 
-    keystone_session = session.Session(auth=keystone_auth, verify=False)
+    return session.Session(auth=keystone_auth, verify=False)
 
-    return client.Client(session=keystone_session)
+def get_keystone_client() -> keystone_client.Client:
+    """
+    Returns a keystone client object.
+    """
+    return keystone_client.Client(session=get_session())
+
+def get_nova_client() -> nova_client.Client:
+    """
+    Returns a nova client object.
+    """
+    return nova_client.Client(2, session=get_session())
+
+def get_neutron_client() -> neutron_client.Client:
+    """
+    Returns a neutron client object.
+    """
+    return neutron_client.Client(session=get_session())
+
+def get_cinder_client() -> cinder_client.Client:
+    """
+    Returns a cinder client object.
+    """
+    return cinder_client.Client("3", session=get_session())
